99ded21d33e4e1bfc0d808085f737812b3c494e8
SamuelAshcroft <samueljashcroft@gmail.com>
2017-12-05 16:46:57 +0000

    This is a note for the commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package ballthreads;
+
+/**
+ *
+ * @author New User
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Dickhead!");
+    }
+}
